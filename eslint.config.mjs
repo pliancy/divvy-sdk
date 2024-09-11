591ece65d7914871723761dee909c0f6f6bd6bd6
Santese Smith <43831817+santese@users.noreply.github.com>
2024-09-11 00:36:37 -0700

    ci: use cjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
 export default [
-    { ignores: ['dist/', 'node_modules/', '.releaserc.js'] },
+    { ignores: ['dist/', 'node_modules/', '.releaserc.cjs'] },
     { files: ['**/*.{js,mjs,cjs,ts}'] },
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
