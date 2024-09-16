fd9d895cc3c0370f53d0d0ffd5afb72ac434b775
Santese Smith <43831817+santese@users.noreply.github.com>
2024-09-16 09:30:39 -0700

    feat: add user api methods

--- src/divvy.ts
+++ src/divvy.ts
@@ -1,13 +1,16 @@
 import { Transactions } from './transactions/transactions'
 import { CustomFields } from './custom-fields/custom-fields'
 import type { DivvyConfig } from './common/common.types'
+import { Users } from './users/users'
 
 export class Divvy {
     transactions: Transactions
     customFields: CustomFields
+    users: Users
 
     constructor(config: DivvyConfig) {
         this.transactions = new Transactions(config)
         this.customFields = new CustomFields(config)
+        this.users = new Users(config)
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Divvy } from './divvy'
 export * from './transactions/transactions.types'
+export * from './transactions/transaction-filters.types'
 export * from './common/common.types'
 export * from './custom-fields/custom-fields.types'
+export * from './users/users.types'
 export { Divvy }
